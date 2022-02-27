bc85f5543d8ffdceebc219f611046eeb83783c49
tmedcount <tmedcount@gmail.com>
2022-02-27 14:52:32 +0900

    고객이 종료를 원할 때까지 실행

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -7,16 +7,20 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("명령어) ");
-		
-		String command = sc.nextLine();
-		
-		if(command.equals("article add")) {
-			System.out.println("== 게시물 등록 ==");
-		} else if(command.equals("article list")) {
-			System.out.println("== 게시물 리스트 ==");
-		} else {
-			System.out.println("== 존재하지 않는 명령어 ==");
+		while(true) {
+			System.out.print("명령어) ");
+			String command = sc.nextLine();
+			
+			if(command.equals("article add")) {
+				System.out.println("== 게시물 등록 ==");
+			} else if(command.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+			} else if(command.equals("system exit")) {
+				System.out.println("== 프로그램 종료 ==");
+				break;
+			} else {
+				System.out.println("== 존재하지 않는 명령어 ==");
+			}
 		}
 		
 		sc.close();
