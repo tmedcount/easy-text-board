e7d2e67b9c90d37850c036ae10bf43a3d772e16a
tmedcount <tmedcount@gmail.com>
2022-02-28 04:19:21 +0900

    게시물 2개까지 저장할 수 있는 기능

--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -4,20 +4,30 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int lastArticleId = 0;
 		
+		int article1_id = 0;
+		String article1_title = "";
+		String article1_body = "";
+		
+		int article2_id = 0;
+		String article2_title = "";
+		String article2_body = "";
+
 		while(true) {
 			System.out.print("명령어) ");
 			String command = sc.nextLine();
 			
-			if(command.equals("article add")) {
+			if(command.equals("system exit")) {
+				System.out.println("== 프로그램 종료 ==");
+				break;
+			} else if(command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 				
 				int id = lastArticleId + 1;
-				String title;
+				String title; 
 				String body;
 				
 				System.out.print("제목 : ");
@@ -25,20 +35,40 @@ public class Main {
 				System.out.print("내용 : ");
 				body = sc.nextLine();
 				
-				System.out.println("== 생성된 게시물 정보 ==");
-				System.out.printf("번호 : %d\n", id);
-				System.out.printf("제목 : %s\n", title);
-				System.out.printf("내용 : %s\n", body);
+				//System.out.printf("제목 : %s, 내용 : %s\n", title, body);
+				
+				if(id == 1) {
+					article1_id = id;
+					article1_title = title;
+					article1_body = body;
+				}
+				
+				if(id == 2) {
+					article2_id = id;
+					article2_title = title;
+					article2_body = body;
+				}
+								
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 				
 				lastArticleId = id;
-				
 			} else if(command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
-			} else if(command.equals("system exit")) {
-				System.out.println("== 프로그램 종료 ==");
-				break;
-			} else {
-				System.out.println("== 존재하지 않는 명령어 ==");
+				
+				if(lastArticleId == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				
+				System.out.println("번호 / 제목");
+				
+				if(lastArticleId >= 1) {
+					System.out.printf("%d / %s\n", article1_id, article1_title);
+				}
+				
+				if(lastArticleId >= 2) {
+					System.out.printf("%d / %s\n", article2_id, article2_title);
+				}
 			}
 		}
 		
