27d968d4062f6d47cd6fe2e7fb2f9d33dc476568
tmedcount <tmedcount@gmail.com>
2022-03-01 01:23:20 +0900

    처음부터 다시 재구현 연습

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -8,41 +8,44 @@ public class App {
 	Article article2 = new Article();
 	
 	public Article getArticle(int id) {
+		
 		if(id == 1) {
 			return article1;
-		} else if (id == 2) {
+		} else if(id == 2) {
 			return article2;
 		}
 		
 		return null;
 	}
-	
+
 	public void run() {
+	
 		Scanner sc = new Scanner(System.in);
 		
+		int maxArticleCount = 2;
 		int lastArticleId = 0;
-
-		while(true) {
+		
+		while(true) {		
 			System.out.print("명령어) ");
 			String command = sc.nextLine();
 			
-			if(command.equals("system exit")) {
-				System.out.println("== 프로그램 종료 ==");
-				break;
-			} else if(command.equals("article add")) {
+			if(command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
 				
+				if(lastArticleId >= maxArticleCount) {
+					System.out.println("더 이상 게시물을 생성할 수 없습니다.");
+					continue;
+				}
+			
 				int id = lastArticleId + 1;
-				String title; 
+				String title;
 				String body;
 				
 				System.out.print("제목 : ");
 				title = sc.nextLine();
 				System.out.print("내용 : ");
-				body = sc.nextLine();
-				
-				//System.out.printf("제목 : %s, 내용 : %s\n", title, body);
-				
+				body = sc.nextLine();	
+						
 				if(id == 1) {
 					article1.id = id;
 					article1.title = title;
@@ -54,48 +57,59 @@ public class App {
 					article2.title = title;
 					article2.body = body;
 				}
-								
+				
 				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 				
 				lastArticleId = id;
+				
 			} else if(command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 				
 				if(lastArticleId == 0) {
-					System.out.println("게시물이 없습니다.");
+					System.out.println("생성된 게시물이 없습니다.");
 					continue;
 				}
 				
 				System.out.println("번호 / 제목");
 				
-				if(lastArticleId >= 1) {
-					System.out.printf("%d / %s\n", article1.id, article1.title);
+				for(int i=1; i<=lastArticleId; i++) {
+					Article article = null;
+					
+					if(i == 1) {
+						article = article1;
+					} else if(i == 2) {
+						article = article2;
+					}
+					
+					System.out.printf("%d / %s\n", article.id, article.title);
+					
 				}
 				
-				if(lastArticleId >= 2) {
-					System.out.printf("%d / %s\n", article2.id, article2.title);
-				}
 			} else if(command.startsWith("article detail ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 				
 				System.out.println("== 게시물 상세 ==");
 				
-				Article selectedArticle = getArticle(inputedId);
-				
-				if(selectedArticle == null || selectedArticle.id == 0) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
+				if(inputedId == 0 || inputedId > lastArticleId) {
+					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
 				
-				System.out.printf("번호 : %d\n", selectedArticle.id);
-				System.out.printf("제목 : %s\n", selectedArticle.title);
-				System.out.printf("내용 : %s\n", selectedArticle.body);
+				Article article = getArticle(inputedId);
+								
+				System.out.printf("번호 : %d\n", article.id);
+				System.out.printf("제목 : %s\n", article.title);
+				System.out.printf("내용 : %s\n", article.body);
 				
+			} else if(command.equals("system exit")) {
+				System.out.print("== 프로그램 종료 ==");
+				break;
+			} else {
+				System.out.println("존재하지 않는 명령어입니다.");
 			}
 		}
 		
 		sc.close();
-	
 		
 	}
 
--- src/com/sbs/example/easytextboard/Main.java
+++ src/com/sbs/example/easytextboard/Main.java
@@ -3,8 +3,9 @@ package com.sbs.example.easytextboard;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
+		new App().run();
 		
-		new App().run();		
 	}
 }
