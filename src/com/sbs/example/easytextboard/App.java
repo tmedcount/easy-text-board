4ad11910d1382e582a779f9a62f5f34adb09efd8
tmedcount <tmedcount@gmail.com>
2022-03-02 15:45:23 +0900

    게시물 상세보기에서 명령어의 숫자 포멧 관련 오류 수정

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -225,7 +225,14 @@ public class App {
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
 			}  else if(command.startsWith("article detail ")) {
-				int inputedId = Integer.parseInt(command.split(" ")[2]);
+				int inputedId = 0;
+				
+				try {
+					inputedId = Integer.parseInt(command.split(" ")[2]);
+				} catch(NumberFormatException e) {
+					System.out.println("게시물 번호를 양의 정수로 입력해 주세요.");
+					continue;
+				}
 				
 				System.out.println("== 게시물 상세 ==");
 								
